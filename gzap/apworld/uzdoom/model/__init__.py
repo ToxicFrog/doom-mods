02e167dea3c3bf9ea42fe3df23028ab375b36b31
Rebecca Kelly <bk@ancilla.ca>
2026-03-20 22:05:54 -0400

    gzap: finalize tuning after logic loading
    This is how it was always meant to work, but earlier refactorings
    swapped the two. Which mostly still worked! But it resulted in the
    location nametable being populated before location disambiguations were
    produced, which meant that any location that required disambiguation
    couldn't be looked up in the table, breaking the client hinting
    mechanism.

--- gzap/apworld/uzdoom/model/__init__.py
+++ gzap/apworld/uzdoom/model/__init__.py
@@ -81,7 +81,6 @@ def init_wad(logic, package, logic_files):
     wadname = logic_files[0].name.split(".")[0]
     with WadLogicLoader(logic, wadname, package) as wadloader:
         wadloader.load_logic(logic_files)
-        get_tuned_wad(wadloader.wad)
 
 def print_header(package):
     if "GZAP_DEBUG" not in os.environ:
@@ -106,6 +105,7 @@ def init_wads(package):
     files = logic_files(package)
     assert len(files) > 0, f'Package {package} contains no logic files'
     init_wad(logic, package, files)
+    get_tuned_wad(logic.wad)
 
     _LOGIC[package] = logic
     return logic
